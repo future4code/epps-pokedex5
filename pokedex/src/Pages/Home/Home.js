06a1089847fb9b2cf7c1bdf21ffff161747b4c6f
Yuri Pinheiro <yuripinheirot@gmail.com>
2021-02-10 17:28:28 -0400

    Iniciando criaçao da pokedex

--- pokedex/src/Pages/Home/Home.js
+++ pokedex/src/Pages/Home/Home.js
@@ -14,14 +14,13 @@ const DivContent = styled.div`
 
 export default function Home() {
   const [listPokemons, setListPokemons] = useState([]);
-  const { PokedexWallet, setPokedexWallet } = usePokedexWallet();
+  const { PokedexWallet, setPokedexWallet } = usePokedexWallet([]);
 
   const getPokemons = () => {
     const url = "https://pokeapi.co/api/v2/pokemon/?limit=20";
     axios
       .get(url)
       .then((res) => {
-        console.log("Request: ", res.data.results);
         setListPokemons(res.data.results);
       })
       .catch((err) => {
@@ -31,7 +30,7 @@ export default function Home() {
 
   const addPokemon = (pokemon) => {
     console.log("Pokemon clicado: ", pokemon);
-    // setPokedexWallet([...PokedexWallet, pokemon]);
+    setPokedexWallet([...PokedexWallet, pokemon]);
 
     let newArray = listPokemons.filter((pokeFilt) => {
       return pokeFilt !== pokemon;
@@ -51,10 +50,11 @@ export default function Home() {
     getPokemons();
   }, []);
 
-  // useEffect(() => {
-  //   console.log("PokedexWallet", PokedexWallet);
-  //   console.log("Listpokemon", listPokemons);
-  // }, [PokedexWallet]);
-
   return <DivContent>{renderPokemons}</DivContent>;
 }
+
+// [
+//   { name: "bulbasaur", url: "https://pokeapi.co/api/v2/pokemon/1/" },
+//   { name: "ivysaur", url: "https://pokeapi.co/api/v2/pokemon/2/" },
+//   { name: "venusaur", url: "https://pokeapi.co/api/v2/pokemon/3/" },
+// ]
