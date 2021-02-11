e730602be04bd64177b7f3b2359dc3ae879ce5c2
Cibertartar <jpleonardo1990@hotmail.com>
2021-02-11 18:50:28 -0300

    details_final

--- pokedex/src/Pages/DetailsPage/DetailsPage.js
+++ pokedex/src/Pages/DetailsPage/DetailsPage.js
@@ -1,72 +1,62 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 import styled from 'styled-components'
 import Header from '../../components/Header/Header'
 import Footer from '../../components/Footer/Footer'
-import DetalhesLayout from '../../components/DetalhesLayout/DetalhesLayout'
+import DetailsLayout from '../../components/DetailsLayout/DetailsLayout'
+import { useParams } from 'react-router-dom'
 
-function DetailsPage (props) {
-  const pokemonId = props
+function DetailsPage() {
+  const { pokemonId } = useParams();
   const BASE_URL = `https://pokeapi.co/api/v2/pokemon/${pokemonId}/`
   const [pokemonData, setPokemonData] = useState({});
-  
-  const pokemon = 
-  {
-    nome: data.forms.name,
-    imagemF: data.sprites.front_default,
-    imagemC: data.sprites.back_default,
-    hp: data.stats[0].base_stat,
-    attack: data.stats[1].base_stat,
-    defense: data.stats[2].base_stat,
-    specialAttack: data.stats[3].base_stat,
-    specialDefense: data.stats[4].base_stat,
-    speed: data.stats[5].base_stat,
-    type1: data.types[0].name,
-    typ2: data.types[1].name,
-    move1: data.moves[0].name,
-    move2: data.moves[1].name,
-    move3: data.moves[2].name
+  const getPokemonDetails = () => {
+    axios
+      .get(BASE_URL)
+      .then((response) => {
+        const { data } = response;
+        setPokemonData(data);
+        console.log(data)
+      })
+      .catch((error) => {
+        console.log(error.message);
+      });
   }
 
-useEffect(() => {
-  axios
-  .get(BASE_URL)
-  .then((response) {
-      const { data } = response;
-      setPokemonData(data);
-    })
-    .catch((error) {
-      console.log(error.message);
-    });
-}, [pokemonId]);
+  useEffect(() => {
+    getPokemonDetails()
+    console.log(pokemonData)
+  }, []);
 
-return (
-  <div>
-    <header>
-    <h1>Nome: {pokemon.nome}</h1>
-    </header>
-    <div class="leftbar">
-      <img src={pokemon.imagemF} alt="pokemon front"/>
-      <img src={pokemon.imagemC} alt="pokemon back"/>
+  return (
+    <div>
+      <header>
+      <h1>Nome: {pokemonData.forms && pokemonData.forms[0].name}</h1> 
+      </header>
+      <div>
+        <img src={pokemonData.sprites && pokemonData.sprites.front_default}/>
+        <img src={pokemonData.sprites && pokemonData.sprites.back_default}/>
+      </div>
+      <div>
+        <h2>Stats</h2>
+        <p>HP: {pokemonData.stats && pokemonData.stats[0].base_stat}</p>
+        <p>Attack: {pokemonData.stats && pokemonData.stats[1].base_stat}</p>
+        <p>Defense: {pokemonData.stats && pokemonData.stats[2].base_stat}</p>
+        <p>Special-attack: {pokemonData.stats && pokemonData.stats[3].base_stat}</p>
+        <p>Special-defense: {pokemonData.stats && pokemonData.stats[4].base_stat}</p>
+        <p>Speed: {pokemonData.stats && pokemonData.stats[5].base_stat}</p>
+      </div>
+      <div>
+        <h3>{pokemonData.types && pokemonData.types[0].type.name}</h3>
+        <h3>{pokemonData.types && pokemonData.types[1].type.name}</h3>
+      </div>
+      <div>
+        <h2>Moves:</h2>
+        <p>{pokemonData.moves && pokemonData.moves[0].move.name}</p>
+        <p>{pokemonData.moves && pokemonData.moves[1].move.name}</p>
+        <p>{pokemonData.moves && pokemonData.moves[2].move.name}</p>
+      </div>
     </div>
-    <div class="midbar">
-      <h2>Stats</h2>
-      <p>HP: {pokemon.hp}</p>
-      <p>Attack: {pokemon.attack}</p>
-      <p>Defense: {pokemon.defense}</p>
-      <p>Special-attack: {pokemon.specialAttack}</p>
-      <p>Special-defense: {pokemon.specialDefense}</p>
-      <p>Speed: {pokemon.speed}</p>
-    </div>
-    <div class="rigthbar1">
-      <h2>{pokemon.type1}</h2>
-      <h2>{pokemon.type2}</h2>
-    </div>
-    <div class="rightbar2">
-    <h2>Moves</h2>
-    <p>{pokemon.move1}</p>
-    <p>{pokemon.move2}</p>
-    <p>{pokemon.move3}</p> 
-  </div>
   );
 }
 
--- pokedex/src/components/DetailsLayout/DetailsLayout.js
+++ pokedex/src/components/DetailsLayout/DetailsLayout.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import styled from 'styled-components'
 
-export default function DetalhesLayout() {
+export default function DetailsLayout() {
   return (
     <div>
       
